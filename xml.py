a3254e29689d1c3fa9701a79b327a6d3fd6bd0c7
litsand <litsand@gmail.com>
2013-04-18 15:41:16 +0800

    1

--- xml.py
+++ xml.py
@@ -4,6 +4,10 @@
 
 import re
 import sys
+import os
+
+
+fdout = open("results.txt","w")
 
 def findfieldCount(line):
     fieldCount = re.match(r"""<entry key=.*fieldCount">(\d*)</entry>""", line)
@@ -28,6 +32,7 @@ def check(linenum, xmllist):
             
         if checkfield(count,xmllist[linenum]) == None:
             print linenum ,'line has a wrong field number'
+            fdout.write(str(linenum)+'line has a wrong field number'+"\n")
             return count
             break
             
@@ -45,34 +50,61 @@ def check(linenum, xmllist):
                 
                 
             
-filename = sys.argv[1]            
+# filename = sys.argv[1]     
 
-xml = open(filename)
-xmllist = list(xml)
+
+# xml = open(filename)
+# xmllist = list(xml)
 
 # print xmllist[20]
 # print len(xmllist[2])
 # print xmllist[2]
 # print len(xmllist)
 
-i = 0
-while True:
-    if i == len(xmllist):
-        print "check complete"
-        break
-    line = xmllist[i]
-    if findfieldCount(line) == None:
-        i = i + 1
-        continue
-    else:
-        count = findfieldCount(line).group(1)
-        i = i + 1
-        if int(check(i, xmllist)) != int(count):
-          
-            print i,"line has a wrong field count"
-            # print count,check(i,xmllist)
+def final_check(filename):
+    # if filename.endswith('xml'):
         
+    xml = open(filename)
+    xmllist = list(xml)
+    #     i = 0
+    # else
+    i = 0
+    fdout.write(filename+"\n")
+    
+    print filename
+    while True:
+        
+        if i == len(xmllist):
+            # print xmllist
+            print "check complete"
+            break
+        line = xmllist[i]
+        if findfieldCount(line) == None:
+            i = i + 1
+            continue
+        else:
+            count = findfieldCount(line).group(1)
+            i = i + 1
+            if int(check(i, xmllist)) != int(count):
+                
+                print i,"line has a wrong field count"
+                fdout.write(str(i)+"line has a wrong field count"+"\n")
+            # print count,check(i,xmllist)
+def main():
+    
+    pre_pwd = os.getcwd()
+    file_list = os.listdir(pre_pwd)
+    # print file_list
+    for file_each in file_list:
+        if file_each.endswith('xml'):
+            final_check(file_each)
+        # else:
+        #     pass
+if __name__ == '__main__':
+    main()
 
+
+                
 #         while true:
             
             
